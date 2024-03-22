1f0e254ac32c578944b5e84d0de74d28398e2d45
shujashah <shuja.ashraf11@gmail.com>
2024-03-22 10:59:04 +0530

    code refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import DateCounter from './components/DateCounter';
 import Header from './components/Header';
+import Main from './components/Main';
 export default function App() {
   return (
     <div className="app">
       <Header />
+      <Main>
+        <p>1-15</p>
+        <p>Question</p>
+      </Main>
     </div>
   );
 }
