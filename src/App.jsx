aeacef589bef6d3ec8aa4410803dcc4c9c989d8d
shujashah <shuja.ashraf11@gmail.com>
2024-03-22 23:14:00 +0530

    implemented the  components and reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
+import { useEffect, useReducer } from 'react';
 import DateCounter from './components/DateCounter';
 import Header from './components/Header';
 import Main from './components/Main';
+import Loader from './components/Loader';
+import Error from './components/Error';
+import StartScreen from './components/StartScreen';
+
+const initialState = {
+  questions: [],
+  //status can be loading, error, ready,
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Action is unknown');
+  }
+}
+
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1-15</p>
-        <p>Question</p>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
