a276dd6fa2070fd52745f9b83215f97febc56783
shujashah <shuja.ashraf11@gmail.com>
2024-03-23 15:35:24 +0530

    implemented restart quiz functionality

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,12 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case 'restart':
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: 'ready',
+      };
     default:
       throw new Error('Action is unknown');
   }
@@ -106,6 +112,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
